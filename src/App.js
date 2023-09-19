d44ca5b88301f33296174f69a3e7d5541657ea40
Liam Shaw <liamjosephshaw@gmail.com>
2023-09-19 14:28:38 +0100

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useState } from 'react';
 import './App.css';
 import Show from './Show';
 
 function App() {
+  const [showName, setShowName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSearch = (e) => {
+    setShowName(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  }
+
+  const handleSearchAgain = () => {
+    setSubmitted(false);
+    setShowName("");
+  }
+
   return (
-    <Show />
+    <div className="App">
+      {!submitted ? (
+        <div className="search">
+          <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleSearch} placeholder="Search show" />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+      ) : (
+        <Show showName={showName} onSearchAgain={handleSearchAgain} />
+      )}
+    </div>
   );
 }
 
--- src/Show.js
+++ src/Show.js
@@ -2,66 +2,81 @@ import React, { useEffect, useState } from 'react';
 const parser = new DOMParser();
 
 async function getShowInfo(name) {
-    return fetch(`https://api.tvmaze.com/singlesearch/shows?q=${name}`)
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${name}`);
+  if (!response.ok) {
+    throw new Error(`No show found with the name "${name}"`);
+  }
+  const data = await response.json();
+  return data;
 }
 
 async function getAmountOfEpisodes(id) {
-    return fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
-    .then(response => response.json())
-    .then(data => data.length) // Return the episode count
-    .catch(error => console.error(error));
+  const response = await fetch(`https://api.tvmaze.com/shows/${id}/episodes`);
+  if (!response.ok) {
+    throw new Error(`No episodes found for the show with id "${id}"`);
+  }
+  const data = await response.json();
+  return data.length;
 }
 
-function Show() {
-    const [show, setShow] = useState(null);
-    const [episodes, setEpisodes] = useState(0);
-    const showName = "Suits"; // Placeholder
+function Show({ showName, onSearchAgain }) {
+  const [show, setShow] = useState(null);
+  const [episodes, setEpisodes] = useState(0);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        getShowInfo(showName).then(data => {
-            setShow(data);
-            getAmountOfEpisodes(data.id).then(episodeCount => {
-                setEpisodes(episodeCount);
-            });
-        });
-    }, [showName]);
+  useEffect(() => {
+    getShowInfo(showName).then(data => {
+      setShow(data);
+      getAmountOfEpisodes(data.id).then(episodeCount => {
+        setEpisodes(episodeCount);
+      }).catch(error => {
+        console.error(error);
+        setError(error.message);
+      });
+    }).catch(error => {
+      console.error(error);
+      setError(error.message);
+    });
+  }, [showName]);
 
-    return show ? (
-        <div>
-            <ShowInfo show={show} />
-            <EpisodeInfo runtime={show.averageRuntime} episodes={episodes} />
-        </div>
-    ) : (
-        <div>Loading...</div>
-    );
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return show ? (
+    <div>
+      <ShowInfo show={show} />
+      <EpisodeInfo runtime={show.averageRuntime} episodes={episodes} />
+      <button onClick={onSearchAgain}>Search Again</button>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 }
 
 function ShowInfo({ show }) {
+  const parsedSummary = parser.parseFromString(show.summary, 'text/html');
+  const textSummary = parsedSummary.body.textContent;
 
-    const parsedSummary = parser.parseFromString(show.summary, 'text/html');
-    const textSummary = parsedSummary.body.textContent;
-
-    return (
-        <div>
-            <h1>{show.name}</h1>
-            <ShowPoster url={show.image.medium} />
-            <p>{ textSummary }</p>
-            <p>Rating: {show.rating.average}</p>
-            <p>Status: {show.status}</p>
-            <p>Average Runtime: {show.averageRuntime}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{show.name}</h1>
+      <ShowPoster url={show.image.medium} />
+      <p>{ textSummary }</p>
+      <p>Rating: {show.rating.average}</p>
+      <p>Status: {show.status}</p>
+      <p>Average Runtime: {show.averageRuntime}</p>
+    </div>
+  );
 }
 
 function ShowPoster({ url }) {
-    return <img src={url} alt="Show poster" />;
+  return <img src={url} alt="Show poster" />;
 }
 
 function EpisodeInfo({ runtime, episodes }) {
-    const totalRuntime = runtime * episodes;
-    return <p>Total Length: {totalRuntime}</p>;
+  const totalRuntime = runtime * episodes;
+  return <p>Total Length: {totalRuntime}</p>;
 }
 
 export default Show;
